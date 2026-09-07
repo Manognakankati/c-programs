63f8d9e3c175b0c83a8eacacb130bb84f2c32aeb
Kankati Manogna <manogna@Kankatis-MacBook-Air.local>
2026-09-07 20:17:37 +0530

    first C program

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    float c,f ;
+    printf("enter temp value in farenheit ln");
+    scanf("%f",&f);
+    c=(f-32)*5.0/9.0;
+    printf("temperature in celsius scale %f,c");
+   
+    return 0 ;
+
+}
